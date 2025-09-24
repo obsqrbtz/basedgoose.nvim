ee6b331efd9129e84e3ecce82d576ff3d4c2cc7b
Daniel Dada <doesdeos@gmail.com>
2025-09-24 15:36:41 +0300

    fixed floating windows bg and borders

--- colors/matugen.lua
+++ colors/matugen.lua
@@ -3,82 +3,91 @@ vim.cmd('syntax reset')
 vim.g.colors_name = 'matugen'
 
 local palette = {
-  base00 = '#0e1415',
-  base01 = '#b44242',
-  base02 = '#95a328',
-  base03 = '#b1cbce',
-  base04 = '#81d4dc',
-  base05 = '#7c435a',
-  base06 = '#a48b4a',
-  base07 = '#dee4e4',
-  base08 = '#899393',
-  base09 = '#81d4dc',
-  base0A = '#b7c7ea',
-  base0B = '#899393',
-  base0C = '#81d4dc',
-  base0D = '#dee4e4',
-  base0E = '#b7c7ea',
-  base0F = '#899393',
-  selectionBg = '#3f4849',
+  background = '#0e1415',
+  onBackground = '#dee4e4',
+  outline = '#899393',
+  primary = '#81d4dc',
+  secondary = '#b1cbce',
+  tertiary = '#b7c7ea',
+  surfaceVariant = '#3f4849',
+  error = '#ffb595',
+  warning = '#e9c16c',
+  success = '#95d5a7',
+  underline = '#ff0000'
 }
 
 local function set_hl(group, opts)  
   vim.api.nvim_set_hl(0, group, opts)
 end
 
+vim.o.winborder = "rounded"
+
 -- Basic highlights
-set_hl('Normal', { fg = palette.base07, bg = palette.base00 })
-set_hl('Comment', { fg = palette.base0F, italic = false })
-set_hl('Constant', { fg = palette.base0C })
-set_hl('String', { fg = palette.base0B })
-set_hl('Identifier', { fg = palette.base0E })
-set_hl('Function', { fg = palette.base0A })
-set_hl('Statement', { fg = palette.base09 })
-set_hl('PreProc', { fg = palette.base0D })
-set_hl('Type', { fg = palette.base04 })
-set_hl('Special', { fg = palette.base03 })
-set_hl('Underlined', { fg = palette.base05 })
-set_hl('Error', { fg = palette.base01, bg = palette.base00 })
-set_hl('Todo', { fg = palette.base00, bg = palette.base03 })
+set_hl('Normal', { fg = palette.  ground, bg = palette.background })
+set_hl('Comment', { fg = palette.outline, italic = false })
+set_hl('Constant', { fg = palette.primary })
+set_hl('String', { fg = palette.outline })
+set_hl('Identifier', { fg = palette.tertiary })
+set_hl('Function', { fg = palette.tertiary })
+set_hl('Statement', { fg = palette.primary })
+set_hl('PreProc', { fg = palette.onBackground })
+set_hl('Type', { fg = palette.primary })
+set_hl('Special', { fg = palette.secondary })
+set_hl('Underlined', { fg = palette.underline })
+set_hl('Error', { fg = palette.error, bg = palette.background })
+set_hl('Todo', { fg = palette.background, bg = palette.secondary })
 
 -- Line numbers
-set_hl('LineNr', { fg = palette.base08 })
-set_hl('CursorLineNr', { fg = palette.base0A })
+set_hl('LineNr', { fg = palette.outline })
+set_hl('CursorLineNr', { fg = palette.tertiary })
 
 -- Cursor and selection
-set_hl('CursorLine', { bg = palette.selectionBg })
-set_hl('Visual', { bg = palette.selectionBg })
+set_hl('CursorLine', { bg = palette.surfaceVariant })
+set_hl('Visual', { bg = palette.surfaceVariant })
 
 -- Status line
-set_hl('StatusLine', { fg = palette.base00, bg = palette.base0E })
-set_hl('StatusLineNC', { fg = palette.base08, bg = palette.base00 })
+set_hl('StatusLine', { fg = palette.background, bg = palette.tertiary })
+set_hl('StatusLineNC', { fg = palette.outline, bg = palette.background })
 
 -- Tabline
-set_hl('TabLine', { fg = palette.base07, bg = palette.base08 })
-set_hl('TabLineSel', { fg = palette.base00, bg = palette.base03 })
+set_hl('TabLine', { fg = palette.onBackground, bg = palette.outline })
+set_hl('TabLineSel', { fg = palette.background, bg = palette.secondary })
+
+-- Floating windows
+
+set_hl('NormalFloat', { bg = "none" })
+set_hl('FloatBorder', { fg = palette.tertiary, bg = "none" })
 
 -- Completion menu
-set_hl('Pmenu', { fg = palette.base07, bg = palette.selectionBg })
-set_hl('PmenuSel', { fg = palette.base00, bg = palette.base0A })
+
+set_hl('Pmenu', { bg = "none" })
+set_hl('PmenuSel', { bg = palette.tertiary, fg = palette.background })
+
+set_hl('BlinkCmpLabel', { bg = "none" })
+set_hl('BlinkCmpMenuSelection', { bg = "none" })
+set_hl('BlinkCmpMenu', { bg = "none" })
+set_hl('BlinkCmpMenuBorder', { fg = palette.tertiary })
+set_hl('BlinkCmpDoc', { bg = "none" })
+set_hl('BlinkCmpDocBorder', { fg = palette.tertiary })
 
 -- Diff
-set_hl('DiffAdd', { fg = palette.base02, bg = palette.base00 })
-set_hl('DiffChange', { fg = palette.base03, bg = palette.base00 })
-set_hl('DiffDelete', { fg = palette.base01, bg = palette.base00 })
-set_hl('DiffText', { fg = palette.base0A, bg = palette.base00 })
+set_hl('DiffAdd', { fg = palette.success, bg = palette.background })
+set_hl('DiffChange', { fg = palette.secondary, bg = palette.background })
+set_hl('DiffDelete', { fg = palette.error, bg = palette.background })
+set_hl('DiffText', { fg = palette.tertiary, bg = palette.background })
 
 -- Diagnostics
-set_hl('DiagnosticError', { fg = palette.base01 })
-set_hl('DiagnosticWarn', { fg = palette.base03 })
-set_hl('DiagnosticInfo', { fg = palette.base04 })
-set_hl('DiagnosticHint', { fg = palette.base0E })
+set_hl('DiagnosticError', { fg = palette.error })
+set_hl('DiagnosticWarn', { fg = palette.warning })
+set_hl('DiagnosticInfo', { fg = palette.primary })
+set_hl('DiagnosticHint', { fg = palette.tertiary })
 
 -- Git signs
-set_hl('GitSignsAdd', { fg = palette.base02 })
-set_hl('GitSignsChange', { fg = palette.base03 })
-set_hl('GitSignsDelete', { fg = palette.base01 })
+set_hl('GitSignsAdd', { fg = palette.success })
+set_hl('GitSignsChange', { fg = palette.secondary })
+set_hl('GitSignsDelete', { fg = palette.error })
 
 -- Treesitter
 set_hl('@string', { link = 'String' })
 set_hl('@function', { link = 'Function' })
-set_hl('@variable', { fg = palette.base07 })
+set_hl('@variable', { fg = palette.onBackground })
